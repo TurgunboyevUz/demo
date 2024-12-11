6a2bfda089add285041d79b14859054ced2a428a
Diyorbek Turg'unboyev <turgunboyev504@gmail.com>
2024-12-11 21:12:34 +0500

    +

--- routes/console.php
+++ routes/console.php
@@ -3,6 +3,15 @@
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 
-Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote')->hourly();
+Artisan::command('tree', function () {
+    $exclude = [
+        'vendor', 'public', 'bootstrap', 'storage', 'config', 'composer.json', 'composer.lock', 'artisan', 'seeders', 'factories'
+    ];
+
+    $path = base_path();
+    $tree = exec("tree --prune -I '" . implode('|', $exclude) . "' $path", $output);
+
+    foreach ($output as $line) {
+        $this->comment($line);
+    }
+});
