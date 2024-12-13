3ac85aac97ad112609951d2d883d6b7f3fb2be61
Diyorbek Turg'unboyev <turgunboyev504@gmail.com>
2024-12-13 16:16:54 +0500

    +

--- resources/views/employee/teacher/achievement.blade.php
+++ resources/views/employee/teacher/achievement.blade.php
@@ -93,4 +93,23 @@
         </div>
     </section>
 </div>
+<div class="modal fade" id="cancelModal" tabindex="-1" role="dialog" aria-labelledby="cancelModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="cancelModalLabel">Bekor Qilish Izohi</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <textarea class="form-control" rows="4" placeholder="Bekor qilish sababini kiriting..."></textarea>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Yopish</button>
+                <button type="button" class="btn btn-primary">Saqlash</button>
+            </div>
+        </div>
+    </div>
+</div>
 @endsection
