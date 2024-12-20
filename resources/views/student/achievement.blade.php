47b5c474c9de6cec8c1a44170a95fe03dc7e6fee
Diyorbek Turg'unboyev <turgunboyev504@gmail.com>
2024-12-20 16:55:32 +0500

    +

--- resources/views/student/achievement.blade.php
+++ resources/views/student/achievement.blade.php
@@ -153,7 +153,15 @@
                         <td>{{ $item->location() }}</td>
                         <td>{{ $item->document_type() }}</td>
                         <td>{{ $item->file->name }}</td>
-                        <td><span class="badge badge-{{ $item->status()['color'] }}">{{ $item->status()['name'] }}</span></td>
+                        <td>
+                            <span class="badge badge-{{ $item->status()['color'] }}">{{ $item->status()['name'] }}</span>
+
+                            @if($item->file->status == 'rejected')
+                            <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#myModal" data-reason="{{ $item->file->reject_reason }}">
+                                <i class="fa fa-eye fa-sm"></i>
+                            </button>
+                            @endif
+                        </td>
 
                         @if($item->file->status == 'pending')
                         <td>
@@ -176,4 +184,39 @@
         </div>
     </div>
 </div>
+
+<div class="modal fade" id="myModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Rad etilish sababi</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <p id="reject-reason"></p>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary btn-close" data-bs-dismiss="modal">Yopish</button>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
+
+@section('scripts')
+<script>
+    $(document).ready(function() {
+        $(".btn-primary").on("click", function() {
+            const reason = $(this).data("reason");
+
+            $("#reject-reason").text(reason || "Rad etilish sababi mavjud emas");
+
+            $("#myModal").modal("show");
+        });
+
+        $(".btn-close, .btn-secondary").on("click", function() {
+            $("#myModal").modal("hide");
+        });
+    });
+</script>
 @endsection
